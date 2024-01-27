68b3edacdccb3bc3513e3f331fdff7fda5651c80
Suggika <156456071+Suggika@users.noreply.github.com>
2024-01-27 14:31:50 +0300

    Add files via upload

---
+++ Setup/script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('startButton');
+const activationKeySelect = document.getElementById('activationKey');
+
+startButton.addEventListener('click', () => {
+  const activationKey = activationKeySelect.value;
+ 
+});
+
+const stopButton = document.getElementById('stopButton');
+const stopKeySelect = document.getElementById('stopKey');
+
+stopButton.addEventListener('click', () => {
+  const stopKey = stopKeySelect.value;
+  
+});
+//Баги, ошибки? Пишите в Discord @suggika
